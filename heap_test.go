ec0f97f320eeae37d23b5e8230cb635312c81304
Steven R Wiley <srwiley@users.noreply.github.com>
2025-07-03 14:19:11 -0700

    Update heap_test.go

--- heap_test.go
+++ heap_test.go
@@ -14,68 +14,85 @@ func TestLevelfunc(t *testing.T) {
 	}
 }
 
-func TestMinMaxLoad(t *testing.T) {
+func writeHeapF(best MinMaxHeap[float64, string]) {
+	iter2 := best.GetIterator(false)
+	for ef, r2 := iter2(); r2 >= 0; ef, r2 = iter2() {
+		fmt.Println("elt", ef, r2)
+	}
+}
 
+func TestMinMaxLoad(t *testing.T) {
 	randomElement := func() HeapElement[float64, string] {
 		key := rand.NormFloat64()
 		return HeapElement[float64, string]{key, "ele: " + fmt.Sprintf("%f", key)}
 	}
 
 	mmHeap := MinMaxHeap[float64, string]{}
-	for j := 0; j < 5; j++ {
-		for i := 0; i < 100; i++ {
+	for i := 0; i < 2; i++ {
+		ele := randomElement()
+		mmHeap.Insert(ele)
+	}
+	for j := 0; j < 1; j++ {
+		for i := 0; i < 200; i++ {
 			ele := randomElement()
-			//ele.Key = float64(j*100 + i)
 			mmHeap.Insert(ele)
 			if (i+2)%20 == 0 {
 				mmHeap.Insert(ele) // add some dups
 			}
+			iter := mmHeap.GetIterator(false)
+			e1, _ := iter()
+			lastKey := e1.Key
+			eCount := 1
+			for e, r := iter(); r >= 0; e, r = iter() {
+				eCount++
+				if lastKey < e.Key {
+					t.Error("Last key less than current key in decending iterator ", lastKey, e.Key, "len", len(mmHeap))
+					writeHeapF(mmHeap)
+					return
+
+				}
+				lastKey = e.Key
+			}
+			if eCount != len(mmHeap) {
+				t.Error("iteration count and heap length not equal", eCount, len(mmHeap))
+			}
 		}
 	}
 
-	iter := mmHeap.GetIterator(false)
-	e1, _ := iter()
-	lastKey := e1.Key
-	for e, r := iter(); r >= 0; e, r = iter() {
-		if lastKey < e.Key {
-			t.Error("Last key less than current key in decending iterator ", lastKey, e)
-		}
-	}
+	for i := 0; i < 30; i++ {
+		iter := mmHeap.GetIterator(false)
+		e1, _ := iter()
+		lastKey := e1.Key
+		eCount := 1
+		for e, r := iter(); r >= 0; e, r = iter() {
+			eCount++
+			if lastKey < e.Key {
+				t.Error("Last key less than current in decending iterator ", lastKey, e.Key, "len", len(mmHeap))
+				writeHeapArrayf(mmHeap)
+				return
 
-	iterA := mmHeap.GetIterator(true)
-	a1, _ := iterA()
-	lastKey = a1.Key
-	for e, r := iterA(); r >= 0; e, r = iterA() {
-		if lastKey > e.Key {
-			t.Error("Last key greater than current key in acending iterator ", lastKey, e)
+			}
+			lastKey = e.Key
 		}
-	}
+		if eCount != len(mmHeap) {
+			t.Error("it count and heap length not equal", eCount, len(mmHeap))
+		}
 
-	ele1 := mmHeap.RemoveMin()
-	lastKey = ele1.Key
-	for i := 0; i < 50; i++ {
-		peek := mmHeap.PeekMin()
-		ele := mmHeap.RemoveMin()
-		if peek != ele {
-			t.Error("PeekMin and RemoveMin do not match ", peek, ele)
+		if i%2 == 0 {
+			peek := mmHeap.PeekMin()
+			ele := mmHeap.RemoveMin()
+			//fmt.Println("removed min", ele)
+			if peek != ele {
+				t.Error("PeekMin and RemoveMin do not match ", peek, ele)
+			}
+			continue
 		}
-		if lastKey > ele.Key {
-			t.Error("Last key greater than current key in RemoveMin ", lastKey, ele)
-		}
-		lastKey = ele.Key
-	}
-	ele1 = mmHeap.RemoveMax()
-	lastKey = ele1.Key
-	for len(mmHeap) > 0 {
 		peek := mmHeap.PeekMax()
 		ele := mmHeap.RemoveMax()
 		if peek != ele {
 			t.Error("PeekMax and RemoveMax do not match ", peek, ele)
 		}
-		if lastKey < ele.Key {
-			t.Error("Last key less than current key in RemoveMax ", lastKey, ele, len(mmHeap))
-		}
-		lastKey = ele.Key
+
 	}
 }
 
@@ -95,6 +112,9 @@ func TestMinMaxHeap(t *testing.T) {
 		mmheap.Insert(v)
 	}
 
+	fmt.Println("Max : ", mmheap.PeekMax())
+	fmt.Println("Min : ", mmheap.PeekMin())
+
 	iter := mmheap.GetIterator(true)
 	e1, _ := iter()
 	lastKey := e1.Key
@@ -102,6 +122,7 @@ func TestMinMaxHeap(t *testing.T) {
 		if lastKey > e.Key {
 			t.Error("Last key greater than current key in ascending iterator ", lastKey, e)
 		}
+		lastKey = e.Key
 	}
 
 	iter = mmheap.GetIterator(false)
@@ -110,6 +131,66 @@ func TestMinMaxHeap(t *testing.T) {
 	for e, r := iter(); r >= 0; e, r = iter() {
 		if lastKey < e.Key {
 			t.Error("Last key less that than current key in decending iterator ", lastKey, e)
+		}
+		lastKey = e.Key
+	}
+}
+
+func writeHeap(best MinMaxHeap[int, string]) {
+	iter2 := best.GetIterator(false)
+	for ef, r2 := iter2(); r2 >= 0; ef, r2 = iter2() {
+		fmt.Println("elt", ef)
+	}
+
+}
+
+func writeHeapArrayf(best MinMaxHeap[float64, string]) {
+	for i, v := range best {
+		fmt.Println(i, ":", v)
+	}
+}
+
+func TestMinMaxHeapI(t *testing.T) {
+	testSet := []HeapElement[int, string]{
+		{0, "a"},
+		{2, "b"},
+		{1, "c"},
+		{1, "d"},
+		{1, "e"},
+		{3, "f"},
+		{1, "g"},
+		{2, "h"},
+		{1, "i"},
+		{1, "j"},
+		{2, "k"},
+		{1, "l"},
+		{2, "m"},
+		{2, "n"},
+		{2, "o"},
+		{3, "p"},
+	}
+
+	mmheap := MinMaxHeap[int, string]{}
+	for _, v := range testSet {
+		mmheap.Insert(v)
+		fmt.Println()
+		writeHeap(mmheap)
+		if len(mmheap) > 6 {
+			mmheap.RemoveMin()
+		}
+		iter := mmheap.GetIterator(false)
+		a1, _ := iter()
+		lastKey := a1.Key
+		toErr := false
+		for e, r := iter(); r >= 0; e, r = iter() {
+			if lastKey < e.Key {
+				t.Error("Last key less that than current key in decending iterator ", lastKey, e)
+				toErr = true
+			}
+			lastKey = e.Key
+		}
+		if toErr {
+			return
 		}
 	}
 }
@@ -147,6 +228,7 @@ func TestMaxHeap(t *testing.T) {
 		if lastKey < ele.Key {
 			t.Error("lastKey < curent during maxHeap remove ", peek, ele)
 		}
+		lastKey = ele.Key
 		fmt.Println(ele)
 	}
 }
@@ -183,6 +265,7 @@ func TestMinHeap(t *testing.T) {
 		if lastKey > ele.Key {
 			t.Error("lastKey < curent during maxHeap remove ", peek, ele)
 		}
+		lastKey = ele.Key
 		fmt.Println(ele)
 	}
 }
@@ -224,4 +307,3 @@ func TestHelpersHeap(t *testing.T) {
 	}
 
 }
-
